80755465982bca0e2c4c68d76175952b5e891bbf
Prayag kad <prayagkad17@gmail.com>
2025-09-03 18:21:57 +0530

    Initial commit

---
+++ src/main/java/com/pm/fsnotes/FsNotesApplication.java
@@ -0,0 +1,13 @@
+package com.pm.fsnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/fsnotes/FsNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.fsnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
